52d71d997c5b51cffb2c01c1765cd316871faa3b
Winston Fassett <winston.fassett@gmail.com>
2025-08-03 20:03:51 -0500

    useref lolfix

--- src/hooks/useWebTorrent.ts
+++ src/hooks/useWebTorrent.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import WebTorrent from 'webtorrent';
 import { TorrentInfo } from '../types';
 import { opfsManager, StoredTorrentMeta } from '../utils/opfs';
